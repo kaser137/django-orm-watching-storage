8cec4f5288eb27bd4ff4c9c403c68201a0b1d7df
Alexander <107580630+kaser137@users.noreply.github.com>
2023-02-08 13:20:27 +0000

    Change creation dicts

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -12,10 +12,11 @@ def passcard_info_view(request, passcode):
     passcard = get_object_or_404(Passcard, passcode=passcode)
     this_passcard_visits = []
     for visit in Visit.objects.filter(passcard=passcard):
-      this_passcard_visit = {}
-      this_passcard_visit['is_strange']=is_visit_long(visit)
-      this_passcard_visit['entered_at']=localtime(visit.entered_at).strftime('%d-%m-%Y %H:%M')
-      this_passcard_visit['duration']=format_duration(get_duration(visit))
+      this_passcard_visit = {
+      'is_strange': is_visit_long(visit),
+      'entered_at': localtime(visit.entered_at).strftime('%d-%m-%Y %H:%M'),
+      'duration': format_duration(get_duration(visit))
+      }
       this_passcard_visits.append(this_passcard_visit)
     context = {
         'passcard': passcard,
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -10,11 +10,12 @@ from django.utils.timezone import localtime
 def storage_information_view(request):
     non_closed_visits = []
     for visit in Visit.objects.filter(leaved_at=None):
-      non_closed_visit = {}
-      non_closed_visit['who_entered']=visit.passcard.owner_name
-      non_closed_visit['is_strange']=is_visit_long(visit)
-      non_closed_visit['entered_at']=localtime(visit.entered_at).strftime('%d-%m-%Y %H:%M')
-      non_closed_visit['duration']=format_duration(get_duration(visit))
+      non_closed_visit = {
+      'who_entered': visit.passcard.owner_name,
+      'is_strange': is_visit_long(visit),
+      'entered_at': localtime(visit.entered_at).strftime('%d-%m-%Y %H:%M'),
+      'duration': format_duration(get_duration(visit))
+      }
       non_closed_visits.append(non_closed_visit)
     context = {
         'non_closed_visits': non_closed_visits,

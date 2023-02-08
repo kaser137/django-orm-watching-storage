173e4d5b3e29529bc12a2666f8e3579a1a940626
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-02-08 22:54:47 +0200

    Edit&format files

--- datacenter/models.py
+++ datacenter/models.py
@@ -3,22 +3,22 @@ from django.utils.timezone import localtime
 
 
 def get_duration(visit):
-  return localtime(visit.leaved_at) - localtime(visit.entered_at)
+    return localtime(visit.leaved_at) - localtime(visit.entered_at)
 
 
 def is_visit_long(visit, minutes=60):
-      duration = get_duration(visit)
-      if duration.seconds > minutes*60:
+    duration = get_duration(visit)
+    if duration.seconds > minutes * 60:
         return True
-      return False 
-  
+    return False
+
 
 def format_duration(duration):
-  seconds = duration.total_seconds()
-  hours = int(seconds // 3600)
-  minutes = int((seconds % 3600) // 60)  
-  format_duration = f'{hours}h {minutes}min'
-  return format_duration
+    seconds = duration.total_seconds()
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    format_duration = f'{hours}h {minutes}min'
+    return format_duration
 
 
 class Passcard(models.Model):
@@ -27,7 +27,6 @@ class Passcard(models.Model):
     passcode = models.CharField(max_length=200, unique=True)
     owner_name = models.CharField(max_length=255)
 
-  
     def __str__(self):
         if self.is_active:
             return self.owner_name
@@ -40,7 +39,6 @@ class Visit(models.Model):
     entered_at = models.DateTimeField()
     leaved_at = models.DateTimeField(null=True)
 
-  
     def __str__(self):
         return '{user} entered at {entered} {leaved}'.format(
             user=self.passcard.owner_name,
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -10,13 +10,13 @@ from django.utils.timezone import localtime
 def storage_information_view(request):
     non_closed_visits = []
     for visit in Visit.objects.filter(leaved_at__isnull=True):
-      non_closed_visit = {
-      'who_entered': visit.passcard.owner_name,
-      'is_strange': is_visit_long(visit),
-      'entered_at': localtime(visit.entered_at).strftime('%d-%m-%Y %H:%M'),
-      'duration': format_duration(get_duration(visit))
-      }
-      non_closed_visits.append(non_closed_visit)
+        non_closed_visit = {
+            'who_entered': visit.passcard.owner_name,
+            'is_strange': is_visit_long(visit),
+            'entered_at': localtime(visit.entered_at).strftime('%d-%m-%Y %H:%M'),
+            'duration': format_duration(get_duration(visit))
+        }
+        non_closed_visits.append(non_closed_visit)
     context = {
         'non_closed_visits': non_closed_visits,
     }

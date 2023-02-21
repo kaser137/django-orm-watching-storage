e3d3ea4f9b4e0a4de292db72d07032e411cabe0c
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-02-22 01:29:28 +0200

    Hide secret info

--- project/settings.py
+++ project/settings.py
@@ -1,26 +1,36 @@
 import os
+from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env(Path('venv', '.env'))
+user = env.str('USER')
+password = env.str('PASSWORD')
+engine = env.str('ENGINE')
+host = env.str('HOST')
+port = env.str('PORT')
+name = env.str('NAME')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'ENGINE': engine,
+        'HOST': host,
+        'PORT': port,
+        'NAME': name,
+        'USER': user,
+        'PASSWORD': password
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = env.str('SECRET_KEY')
 
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
 ROOT_URLCONF = 'project.urls'
 
 ALLOWED_HOSTS = ['*']
-
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
@@ -30,7 +40,6 @@ TEMPLATES = [
         'APP_DIRS': True,
     },
 ]
-
 
 USE_L10N = True
 
